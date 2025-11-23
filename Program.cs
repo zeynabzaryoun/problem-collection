4d2f174fbec57318f0e3c84b74e8582feaada38e
zeynabzaryoun <92928382+zeynabzaryoun@users.noreply.github.com>
2025-11-24 00:51:07 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using leetcode.problems;
+
+class Program {
+    public static void Main(String[] args)
+    {
+        //Problem1
+        string answer = Problem1.MergeAlternately("ab", "pqrs");
+        Console.WriteLine(answer);
+
+        Console.ReadKey();
+    }
+}
